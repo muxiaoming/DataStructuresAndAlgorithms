6b65ea20c6100f6d83f065316c164329c702cb1d
muxiaoming <405627878@qq.com>
2022-12-04 17:12:19 +0800

    sparse array

---
+++ Algorithms/src/main/java/com/java/Main.java
@@ -0,0 +1,12 @@
+package com.java;
+
+/**
+ * @Author: code muxiaoming
+ * @DateCreatedIn: ${DATE} ${TIME}
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ DataStructures/src/main/java/com/java/sparsearray/SparseArray.java
@@ -0,0 +1,6 @@
+package com.java.sparsearray;/**
+ * @Author: code muxiaoming
+ * @DateCreatedIn: 2022/12/1 20:43
+ * @Description:
+ */public class SparseArray {
+}
